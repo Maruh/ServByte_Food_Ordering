6a3fd816f4574c4161d21df5e82214fa9d945ee0
Maruh Akporowho <32528793+Maruh@users.noreply.github.com>
2022-02-01 03:38:05 +0100

    Delete BACK END/SITHUMINA directory

--- BACK END/SITHUMINA/sithumina-transaction/src/main/java/com/lhu/transaction/Dao/TransactiondataDao.java
+++
@@ -1,9 +0,0 @@
-package com.lhu.transaction.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lhu.transaction.model.Transactiondata;
-
-public interface TransactiondataDao extends JpaRepository<Transactiondata, Integer>{
-
-}
